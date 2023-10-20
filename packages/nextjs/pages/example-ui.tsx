30f394250a7fa2b02a3089a4edac7ca42b3552b1
codechefsong <133816663+codechefsong@users.noreply.github.com>
2023-10-20 14:45:40 -0400

    add the layout of the board

--- packages/nextjs/pages/example-ui.tsx
+++ packages/nextjs/pages/example-ui.tsx
@@ -1,9 +1,11 @@
+import { BOARD_STYLES } from "../components/board/Names";
 import type { NextPage } from "next";
+import { useAccount } from "wagmi";
 import { MetaHeader } from "~~/components/MetaHeader";
-import { ContractData } from "~~/components/example-ui/ContractData";
-import { ContractInteraction } from "~~/components/example-ui/ContractInteraction";
 
 const ExampleUI: NextPage = () => {
+  const { address } = useAccount();
+
   return (
     <>
       <MetaHeader
@@ -14,9 +16,38 @@ const ExampleUI: NextPage = () => {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree&display=swap" rel="stylesheet" />
       </MetaHeader>
-      <div className="grid lg:grid-cols-2 flex-grow" data-theme="exampleUi">
-        <ContractInteraction />
-        <ContractData />
+      <div className="ml-6" data-theme="exampleUi">
+        <div className="flex">
+          <div>
+            <h2 className="mt-4 text-3xl">Board</h2>
+            <p>{address}</p>
+            <button
+              className="py-2 px-16 mb-1 mt-3 mr-3 bg-green-500 rounded baseline hover:bg-green-300 disabled:opacity-50"
+              onClick={() => console.log("play")}
+            >
+              Play
+            </button>
+            <button
+              className="py-2 px-16 mb-1 mt-3 mr-3 bg-green-500 rounded baseline hover:bg-green-300 disabled:opacity-50"
+              onClick={() => console.log("roll")}
+            >
+              Roll
+            </button>
+            <div className="relative mt-10" style={{ width: "450px", height: "600px" }}>
+              {BOARD_STYLES &&
+                BOARD_STYLES.map((item, index) => (
+                  <div
+                    key={index}
+                    className={
+                      "w-20 h-20 border border-gray-300 font-bold bg-white" + " " + BOARD_STYLES[index] || "grid-1"
+                    }
+                  >
+                    {item}
+                  </div>
+                ))}
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
