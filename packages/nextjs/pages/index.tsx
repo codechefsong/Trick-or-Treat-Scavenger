a088e8649a608b465f30aa668455f49b917f51bc
codechefsong <133816663+codechefsong@users.noreply.github.com>
2023-11-05 08:48:33 -0500

    change the home page

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,68 +1,48 @@
+import Image from "next/image";
 import Link from "next/link";
 import type { NextPage } from "next";
-import { BugAntIcon, MagnifyingGlassIcon, SparklesIcon } from "@heroicons/react/24/outline";
 import { MetaHeader } from "~~/components/MetaHeader";
 
 const Home: NextPage = () => {
   return (
     <>
       <MetaHeader />
-      <div className="flex items-center flex-col flex-grow pt-10">
+      <div className="flex items-center flex-col flex-grow pt-10 bg-zinc-800 text-white">
         <div className="px-5">
           <h1 className="text-center mb-8">
             <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
+            <span className="block text-4xl font-bold"> Trick or Treat Scavenger</span>
           </h1>
-          <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/pages/index.tsx
-            </code>
-          </p>
-          <p className="text-center text-lg">
-            Edit your smart contract{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
-            </code>{" "}
-            in{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
-            </code>
-          </p>
+          <Image className="ml-8" alt="Game" width={500} height={350} src="/assets/game.png" />
+          <p className="text-center text-lg">A Halloween NFT board game</p>
+          <div className="flex justify-center mb-2">
+            <Link
+              href="/example-ui"
+              passHref
+              className=" py-2 px-16 mb-1 mt-3 bg-orange-500 rounded baseline hover:bg-orange-400 disabled:opacity-50"
+            >
+              Play
+            </Link>
+          </div>
         </div>
 
-        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
-          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contract
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <SparklesIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Experiment with{" "}
-                <Link href="/example-ui" passHref className="link">
-                  Example UI
-                </Link>{" "}
-                to build your own UI.
-              </p>
-            </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Explore your local transactions with the{" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
+        <div className="flex-grow bg-orange-500 w-full mt-16 px-8 py-12">
+          <div className="text-center">
+            <h2 className="mt-3 text-4xl">How to play?</h2>
+          </div>
+          <div className="flex justify-center">
+            <ul className="list-disc" style={{ width: "600px" }}>
+              <li>Player can roll a four-sided dice to move</li>
+              <li>If you land on a house space, you can mint 10 candies as ERC20 tokens</li>
+              <li>If you land on a thief space, you have to pay the thief with candies</li>
+              <li>You can hire a thief to steal other players&apos; candies.</li>
+            </ul>
+          </div>
+          <p className="text-3xl text-center">Requirements</p>
+          <div className="flex justify-center">
+            <ul className="list-disc" style={{ width: "600px" }}>
+              <li>To play, you must own a Pumpkin Buckets NFT to play</li>
+            </ul>
           </div>
         </div>
       </div>
